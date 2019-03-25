a1b617b80ccfed4d99a21ef011de68c3cc694c36
vega2k <vega2k@naver.com>
2019-03-26 07:42:41 +0900

    Simple SmartContract Web3 Client

--- contracts/Coursetro .sol
+++ contracts/Coursetro .sol
@@ -2,21 +2,6 @@ pragma solidity ^0.4.25;
 
 contract Coursetro {
     
-   string fName;
-   uint age;
-   
-   constructor() public {
-       fName = 'vega2k';
-       age = 10;
-   }
 
-   function setInstructor(string _fName, uint _age) public {
-       fName = _fName;
-       age = _age;
-   }
-   
-   function getInstructor() public view returns (string, uint) {
-       return (fName, age);
-   }
     
 }
